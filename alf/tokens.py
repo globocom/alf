4c624b5ba20f1c8c719bf6f4d6aa3f0934d08509
Vinícius Chagas <vinimaster@gmail.com>
2013-09-20 01:36:01 -0300

    Simplifying Token class

--- alf/tokens.py
+++ alf/tokens.py
@@ -15,28 +15,11 @@ class Token(object):
         self._access_token = access_token
         self._expires_in = expires_in
 
-        self._update_expires_on()
+        self._expires_on = datetime.now() + timedelta(seconds=self._expires_in)
 
     @property
     def access_token(self):
         return self._access_token
 
-    @access_token.setter
-    def access_token(self, access_token):
-        self._access_token = access_token
-
     def is_valid(self):
         return self._expires_on >= datetime.now()
-
-    @property
-    def expires_in(self):
-        return self._expires_in
-
-    @expires_in.setter
-    def expires_in(self, expires_in):
-        self._expires_in = expires_in
-
-        self._update_expires_on()
-
-    def _update_expires_on(self):
-        self._expires_on = datetime.now() + timedelta(seconds=self.expires_in)
