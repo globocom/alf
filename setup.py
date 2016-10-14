8dcfcad7d851c8fb89aac7bdd28814b5d068c040
Diogo Munaro Vieira <diogo.munaro@corp.globo.com>
2016-10-14 19:14:09 -0300

    Bump to 0.7.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alf',
-    version='0.7.0',
+    version='0.7.1',
     description="OAuth Client",
     long_description=open('README.rst').read(),
     keywords='oauth client client_credentials requests',
